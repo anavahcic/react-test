f019f336e7b26ca8a601398a72047fe744703cb4
Ana Vahcic <anavahcic@Anas-MacBook-Air.local>
2022-11-28 18:19:43 +0100

    lorem deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This is a production branch</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, expedita.</h2>
     </div>
   );
 }
